73a5f118e60c5d8ac2ffa65d4994ed6d68f3efa8
ayuub <ayoubbenomar1230@gmail.com>
2026-01-09 11:42:35 +0100

    Update file: DocumentService.java

--- src/main/java/org/ayoub/barakapay/service/DocumentService.java
+++ src/main/java/org/ayoub/barakapay/service/DocumentService.java
@@ -1,4 +1,9 @@
 package org.ayoub.barakapay.service;
 
+import org.ayoub.barakapay.model.dto.DocumentDto;
+import org.springframework.web.multipart.MultipartFile;
+
 public interface DocumentService {
+    public DocumentDto uploadDocument(Integer operationId, MultipartFile file);
+
 }

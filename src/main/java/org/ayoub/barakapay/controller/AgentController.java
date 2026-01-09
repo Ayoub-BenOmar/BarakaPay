0811321e98452fc5ccd93f74229867f340292e94
ayuub <ayoubbenomar1230@gmail.com>
2026-01-09 11:42:31 +0100

    Update file: AgentController.java

--- src/main/java/org/ayoub/barakapay/controller/AgentController.java
+++ src/main/java/org/ayoub/barakapay/controller/AgentController.java
@@ -1,5 +1,31 @@
 package org.ayoub.barakapay.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.ayoub.barakapay.model.dto.OperationDto;
+import org.ayoub.barakapay.service.impl.OperationsServiceImpl;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/agent")
+@RequiredArgsConstructor
 public class AgentController {
+
+    private final OperationsServiceImpl operationsService;
+
+    @GetMapping("/operations/pending")
+    public List<OperationDto> getPendingOperations() {
+        return operationsService.getPendingOperations();
+    }
+
+    @PutMapping("/operations/{id}/approve")
+    public void approveOperation(@PathVariable Integer id) {
+        operationsService.approveOperation(id);
+    }
+
+    @PutMapping("/operations/{id}/reject")
+    public void rejectOperation(@PathVariable Integer id) {
+        operationsService.rejectOperation(id);
+    }
 }

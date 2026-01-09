e22e48db5155734f7aa48bb704220dd8467efda8
ayuub <ayoubbenomar1230@gmail.com>
2026-01-09 11:42:35 +0100

    Update file: OperationService.java

--- src/main/java/org/ayoub/barakapay/service/OperationService.java
+++ src/main/java/org/ayoub/barakapay/service/OperationService.java
@@ -2,13 +2,18 @@ package org.ayoub.barakapay.service;
 
 import org.ayoub.barakapay.enums.OperationStatus;
 import org.ayoub.barakapay.model.dto.OperationDto;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.List;
 
 public interface OperationService {
-    OperationDto deposit(Integer accountId, BigDecimal amount);
-    OperationDto withdraw(Integer accountId, BigDecimal amount);
-    OperationDto transfer(Integer fromAccountId, Integer toAccountId, BigDecimal amount);
-    OperationDto getAllOperations();
-    void updateOperationStatus(Integer operationId, OperationStatus status);
+    OperationDto deposit(BigDecimal amount) throws IOException;
+    OperationDto withdraw(BigDecimal amount);
+    OperationDto transfer(String toAccount, BigDecimal amount);
+    List<OperationDto> getAllOperations();
+    List<OperationDto> getPendingOperations();
+    void approveOperation(Integer operationId);
+    void rejectOperation(Integer operationId);
 }

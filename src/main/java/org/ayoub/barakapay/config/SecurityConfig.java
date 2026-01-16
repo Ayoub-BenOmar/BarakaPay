d2bdee2e3d7efbb90d65983a46ca37d2bd195300
ayuub <ayoubbenomar1230@gmail.com>
2026-01-16 14:55:53 +0100

    commit

--- src/main/java/org/ayoub/barakapay/config/SecurityConfig.java
+++ src/main/java/org/ayoub/barakapay/config/SecurityConfig.java
@@ -23,8 +23,6 @@ import org.springframework.security.oauth2.server.resource.authentication.JwtGra
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 
 @Configuration
 @RequiredArgsConstructor
